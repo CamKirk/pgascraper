9fcb9325fccdbcb9e0672690fce84d246b7322de
camkirk <camden.kirkland@gmail.com>
2017-11-12 14:39:36 -0600

    working scrape

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const termList = require('./terms.js');
 const fs = require('fs');
 const request = require('request');
 
-request('https://www.wikipedia.org/wiki/Javascript', (err, res, body)=>{
-  //if err throw err;
-  //console.log(res);
-  //console.log(body);
+let scrapedArray = [];
 
-  $ = cheerio.load(body);
-  console.log($('div.mw-parser-output > p').first())
+
+
+termList.terms.forEach((term)=>{
+  request('https://www.wikipedia.org/wiki/' + term, (err, res, body)=>{
+    if (err) throw err;
+
+    $ = cheerio.load(body);
+    //console.log($('div.mw-parser-output > p').first().html())
+    const definition = $('div.mw-parser-output > p').first().text();
+    const output = {[term] : definition};
+    scrapedArray.push(output);
+    if (scrapedArray.length == termList.terms.length){
+      fs.appendFile('results.json', JSON.stringify(scrapedArray,null,'\t'), (err)=>{
+      console.log('appended file');
+    })
+    }
+  });
 });
+
+
+
+
 
 //loop over terms.js
 //use request to poll wikipedia page
--- terms.js
+++ terms.js
@@ -1,1 +1,34 @@
-module.exports.terms = ['Javascript','JQuery']
+module.exports.terms = ['Javascript',
+'JQuery',
+'High-level_programming_language',
+'Dynamic_programming_language',
+'Prototype-based_programming',
+'Cascading_Style_Sheets',
+'HTML',
+'reactjs',
+'angularjs',
+'model-view-controller',
+'Java',
+'C#',
+'http',
+'https',
+'json',
+'Server_(computing)',
+'mongodb',
+'MySQL',
+'Relational_database_management_system',
+'object-oriented programming',
+'functional programming',
+'imperative programming',
+'internet',
+'R',
+'data science',
+'Immutable_object',
+'Object_(computer_science)',
+'Array_data_type'
+'Computer_virus',
+
+
+
+
+]
