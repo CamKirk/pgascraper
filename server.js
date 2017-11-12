cc88af13935d9f360e05ece9645fae82c927e6f0
camkirk <camden.kirkland@gmail.com>
2017-11-12 13:35:58 -0600

    working on request

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const termList = require('./terms.js');
 const fs = require('fs');
 const request = require('request');
 
+request('https://www.wikipedia.org/wiki/Javascript', (err, res, body)=>{
+  //if err throw err;
+  //console.log(res);
+  //console.log(body);
+
+  $ = cheerio.load(body);
+  console.log($('div.mw-parser-output > p').first())
+});
+
 //loop over terms.js
 //use request to poll wikipedia page
 //use cheerio.load to init token
 //use .first to grab first <p> tag after the header
-//store data into a array of objects 
+//store data into a array of objects
---
+++ terms.js
@@ -0,0 +1,1 @@
+module.exports.terms = ['Javascript','JQuery']
