770e4ecf475e11d085ec25e8452c19905453aeb2
camkirk <camden.kirkland@gmail.com>
2018-02-16 12:06:44 -0600

    commenting blocks

--- server.js
+++ server.js
@@ -11,28 +11,34 @@ termList.terms.forEach((term)=>{
   request('https://www.wikipedia.org/wiki/' + term, (err, res, body)=>{
     if (err) throw err;
 
+    //replace underscores with spaces
     if (term.includes('_')){
       term = term.replace(/_/g, ' ');
     };
 
+    //remove parenthetical information
     if (term.includes('(')){
       term = term.replace(/\(.*\)/,"");
     };
+
 
     $ = cheerio.load(body);
     //console.log($('div.mw-parser-output > p').first().html())
     let definition = $('div.mw-parser-output > p').first().text();
 
+    //remove prononciation and non-english characters in the first paragraph definition
     if (definition.includes('[')){
       definition = definition.replace(/\[.*\]/g,"");
     };
 
-     scrapedObject[term] = definition;
+    scrapedObject[term] = definition;
      
+    //on terminus of forEach, write a json file form 
     if (Object.keys(scrapedObject).length == termList.terms.length){
       fs.appendFile('results.json', JSON.stringify(scrapedObject,null,'\t'), (err)=>{
       console.log('appended file');
     })
+    
     }
   });
 });
